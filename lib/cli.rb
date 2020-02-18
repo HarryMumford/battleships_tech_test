cd6c604761b4d8187d40fb16f58874a5596eb85d
Harry Mumford <harry_mumford@hotmail.co.uk>
2020-02-18 23:01:36 +0000

    feat: test drive the choosing ship prompt

---
+++ lib/cli.rb
@@ -0,0 +1,6 @@
+class CLI
+  CHOOSE_SHIP = "To place ship: type 'v, h' for vertical or horizontal '1-4' for size followed by the coordinates (a-j, 1-8) e.g. v1a"
+  def run
+    puts CHOOSE_SHIP
+  end
+end
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,4 +1,11 @@
+require 'cli'
+
 describe CLI do
   describe '#run' do
-    it 'takes the user input ""'
+    it 'outputs a prompt for placing a ship' do
+      cli = CLI.new
+      prompt = "To place ship: type 'v, h' for vertical or horizontal '1-4' for size followed by the coordinates (a-j, 1-8) e.g. v1a\n"
+      expect{ cli.run }.to output(prompt).to_stdout
+    end
+  end
 end
