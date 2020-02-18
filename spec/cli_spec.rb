b39e1c5c2d502ce4f74f0403a221009d8d5c3c0d
Harry Mumford <harry_mumford@hotmail.co.uk>
2020-02-18 22:35:07 +0000

    refactor: change storage method to array

---
+++ spec/cli_spec.rb
@@ -0,0 +1,4 @@
+describe CLI do
+  describe '#run' do
+    it 'takes the user input ""'
+end
--- spec/feature/place_single_ship_spec.rb
+++ spec/feature/place_single_ship_spec.rb
@@ -1,9 +1,9 @@
-# require 'game'
+require 'game'
 
 # describe 'feature: place single ship' do
 #   it 'scenario: player chooses a single ship in horizontal position' do
+#     skip
 #     view = "s----------\n-----------\n-----------\n-----------\n-----------\n-----------\n-----------\n-----------\n-----------\n-----------\n"
-#     puts view
 #     game = Game.new
 #     game.place(ship)
 #     expect { player_1.place(ship) }.to output(view).stdout
